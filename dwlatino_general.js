a8bfaedc6fe5ff2891d3a33870512f426f598664
dwlatino <dwlatino.ven@gmail.com>
2022-02-15 17:43:35 -0400

    15feb 2022

--- dwlatino_general.js
+++ dwlatino_general.js
@@ -10,14 +10,14 @@ contenidoCabecera.innerHTML =
 </a>
 <nav id="navegacion_principal">
   <ul>
-    <a href="#curso_html">				
+    <a href="#cursos_pie_de_pagina">				
       <li>
       CURSOS
       </li>
     </a>
-    <a href="#bio">				
+    <a href="#bio_pie_de_pagina">				
       <li>
-      EQUIPO
+      INFORMACIÓN
       </li>
     </a>
     <a href="#pie_de_pagina">				
@@ -30,9 +30,9 @@ contenidoCabecera.innerHTML =
 `
 contenidoPieDePagina.innerHTML =
 `
-<p>
+<h2>
 CONTÁCTANOS
-</p>
+</h2>
 <p>
 Correo: dwlatino.ven@gmail.com
 </p>
@@ -45,4 +45,51 @@ Whatsapp: +58 0414 728 1033
 <p>
 GitHub: dwlatino
 </p>
+<br>
+<p></p>
+<h2 id="cursos_pie_de_pagina">
+CURSOS
+</h2>
+<a href="cursos_html.html">
+	<p>
+	Tu primer HTML
+	</p>
+</a>
+<a href="cursos_html.html">
+	<p>
+	Avanzando en HTML
+	</p>
+</a>
+<a href="cursos_html.html">
+	<p>
+	HTML intensivo
+	</p>
+</a>
+<br>
+<p></p>
+<h2 id="bio_pie_de_pagina">
+BIO
+</h2>
+<img src="foto_personal.png" alt="foto personal" width="100%">
+<p>
+Estudié arquitectura en la Universidad de los Andes, enfocándome en áreas 
+de digitalización y modelado de edificaciones.
+</p>
+<p>
+Mi trasfondo técnico-artístico junto a mi interés en el 
+trabajo remoto me guió a la programación web.
+</p>
+<p>
+Aprendí front-end development a través de tutoriales, 
+aplicaciones móviles y mucho "sudor y lágrimas".
+</p>
+<p>
+Me apasiona aprender y guiar a otros en su proceso de 
+aprendizaje. Puedo impulsarte en tu proyecto de construirte en un front-end developer.
+</p>
+<p>
+contáctame a través de la información publicada en el incio de éste 
+pie de página.
+</p>
+
 `
