2cb1ab75ae0463759feefeb07dbc2288132ec86b
dwlatino <dwlatino.ven@gmail.com>
2022-02-16 12:05:32 -0400

    15feb 2022

--- dwlatino_general.js
+++ dwlatino_general.js
@@ -1,95 +1,72 @@
 const contenidoCabecera = document.getElementById("cabecera");
 const contenidoPieDePagina = document.getElementById("pie_de_pagina");
+const navegacionMovil = document.getElementById("navegacion_movil");
 
 contenidoCabecera.innerHTML =
 `
 <a href="index.html">
-<div id="logo">
-  <img src="logo.svg">
-</div>
+	<div id="contenedor_logo">
+	  <img src="logo.svg" alt="Logo de dwlatino" style="max-width:300px">
+	</div>
 </a>
+<div id="boton_menu_cabecera">
+	<div></div>
+	<div></div>
+	<div></div>
+</div>
 <nav id="navegacion_principal">
-  <ul>
-    <a href="#cursos_pie_de_pagina">				
-      <li>
-      CURSOS
-      </li>
-    </a>
-    <a href="#bio_pie_de_pagina">				
-      <li>
-      INFORMACIÓN
-      </li>
-    </a>
-    <a href="#pie_de_pagina">				
-      <li>
-      CONTACTO
-      </li>
-    </a>
-  </ul>
+	<ul>
+		<a href="#cursos_pie_de_pagina">				
+			 <li>CURSOS</li>
+		 </a>
+		<a href="#contacto_pie_de_pagina">				
+			 <li>CONTACTO</li>
+		</a>
+	</ul>
 </nav>
 `
+
+navegacionMovil.innerHTML = 
+`
+<nav
+	<ul>
+		<a href="#cursos_pie_de_pagina">				
+			 <li>CURSOS</li>
+		 </a>
+		<a href="#contacto_pie_de_pagina">				
+			 <li>CONTACTO</li>
+		</a>
+	</ul>
+</nav>
+`
+const botonMenuCabecera = document.getElementById("boton_menu_cabecera");
+botonMenuCabecera.addEventListener('click', function() {
+		if(navegacionMovil.style.display == 'block') {
+			navegacionMovil.style.display = 'none';
+		} else {
+			navegacionMovil.style.display = 'block';}
+	}
+);
+
 contenidoPieDePagina.innerHTML =
 `
-<h2>
-CONTÁCTANOS
-</h2>
-<p>
-Correo: dwlatino.ven@gmail.com
-</p>
-<p>
-Teléfono: +58 0414 728 1033
-</p>
-<p>
-Whatsapp: +58 0414 728 1033
-</p>
-<p>
-GitHub: dwlatino
-</p>
-<br>
-<p></p>
-<h2 id="cursos_pie_de_pagina">
-CURSOS
-</h2>
-<a href="cursos_html.html">
-	<p>
-	Tu primer HTML
-	</p>
+<h2 id="cursos_pie_de_pagina">CURSOS</h2>
+<a href="cursos_html_1.html">
+	<p>1. Tu primer HTML</p>
 </a>
-<a href="cursos_html.html">
-	<p>
-	Avanzando en HTML
-	</p>
-</a>
-<a href="cursos_html.html">
-	<p>
-	HTML intensivo
-	</p>
+<a href="cursos_html_2.html">
+	<p>2. Avanzando en HTML</p>
 </a>
 <br>
-<p></p>
-<h2 id="bio_pie_de_pagina">
-BIO
-</h2>
-<img src="foto_personal.png" alt="foto personal" width="100%">
-<p>
-Estudié arquitectura en la Universidad de los Andes, enfocándome en áreas 
-de digitalización y modelado de edificaciones.
-</p>
-<p>
-Mi trasfondo técnico-artístico junto a mi interés en el 
-trabajo remoto me guió a la programación web.
-</p>
-<p>
-Aprendí front-end development a través de tutoriales, 
-aplicaciones móviles y mucho "sudor y lágrimas".
-</p>
-<p>
-Me apasiona aprender y guiar a otros en su proceso de 
-aprendizaje. Puedo impulsarte en tu proyecto de construirte en un front-end developer.
-</p>
-<p>
-contáctame a través de la información publicada en el incio de éste 
-pie de página.
-</p>
 
+<img src="foto_personal.png" alt="foto personal" style="max-width:250px">
+<p>Me apasiona aprender y guiar a otros en su proceso de 
+aprendizaje. Puedo impulsarte en tu proyecto de construirte en un front-end developer.</p>
+<p>Contáctame a través de la información que encontrarás más abajo.</p>
+
+<h2 id="contacto_pie_de_pagina">CONTACTO</h2>
+<p>Correo: dwlatino.ven@gmail.com</p>
+<p>Teléfono: +58 0414 728 1033</p>
+<p>Whatsapp: +58 0414 728 1033</p>
+<p>GitHub: dwlatino</p>
 `
