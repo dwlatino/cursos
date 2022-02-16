12f9d66c595213a6a5fb3111b7be6fd86a68ee87
dwlatino <dwlatino.ven@gmail.com>
2022-02-16 19:04:15 -0400

    16feb 2022

--- dwlatino_cursos.js
+++ dwlatino_cursos.js
@@ -1,6 +1,20 @@
 const navegacionCurso1 = document.getElementById("navegacion_curso_1");
+const navegacionSecundariaCurso1 = document.getElementById("navegacion_secundaria_curso_1");
 
 navegacionCurso1.innerHTML = 
+`
+<h1>Menú de curso</h1>
+	<ul>
+		<a href="curso_html_1_1.html"><li>1. Tu primer documento HTML</li></a>
+		<a href="curso_html_1_2.html"><li>2. Tus primeros elementos en HTML</li></a>
+		<a href="curso_html_1_3.html"><li>3. Tu primer "head" en HTML</li></a>
+		<a href="#"><li>4. Tu primer "body" en HTML</li></a>
+		<a href="#"><li>5. Tu primer formateado en HTML</li></a>
+		<a href="#"><li>6. Tu primera web en HTML</li></a>
+	</ul>
+`
+
+navegacionSecundariaCurso1.innerHTML = 
 `
 <h1>Menú de curso</h1>
 	<ul>
