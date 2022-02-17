ca08ea86bf1142be90ace41a7193fd5b0389d7ea
dwlatino <dwlatino.ven@gmail.com>
2022-02-17 14:06:27 -0400

    17feb 2022

--- js_dwlatino_cursos.js
+++ js_dwlatino_cursos.js
@@ -8,7 +8,7 @@ navegacionCurso1.innerHTML =
 		<a href="curso_html_1_1.html"><li>1. Tu primer documento HTML</li></a>
 		<a href="curso_html_1_2.html"><li>2. Tus primeros elementos en HTML</li></a>
 		<a href="curso_html_1_3.html"><li>3. Tu primer "head" en HTML</li></a>
-		<a href="#"><li>4. Tu primer "body" en HTML</li></a>
+		<a href="curso_html_1_4.html"><li>4. Tu primer "body" en HTML</li></a>
 		<a href="#"><li>5. Tu primer formateado en HTML</li></a>
 		<a href="#"><li>6. Tu primera web en HTML</li></a>
 	</ul>
@@ -21,7 +21,7 @@ navegacionSecundariaCurso1.innerHTML =
 		<a href="curso_html_1_1.html"><li>1. Tu primer documento HTML</li></a>
 		<a href="curso_html_1_2.html"><li>2. Tus primeros elementos en HTML</li></a>
 		<a href="curso_html_1_3.html"><li>3. Tu primer "head" en HTML</li></a>
-		<a href="#"><li>4. Tu primer "body" en HTML</li></a>
+		<a href="curso_html_1_4.html"><li>4. Tu primer "body" en HTML</li></a>
 		<a href="#"><li>5. Tu primer formateado en HTML</li></a>
 		<a href="#"><li>6. Tu primera web en HTML</li></a>
 	</ul>
